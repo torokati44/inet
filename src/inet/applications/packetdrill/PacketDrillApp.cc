923defca425448448c9d581618c586fd3035a154
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-10-03 19:34:59 +0200

    TMP - UDP uses FlatPacket - not completed

--- src/inet/applications/packetdrill/PacketDrillApp.cc
+++ src/inet/applications/packetdrill/PacketDrillApp.cc
@@ -1128,8 +1128,8 @@ bool PacketDrillApp::compareDatagram(IPv4Datagram *storedDatagram, IPv4Datagram 
     }
     switch (storedDatagram->getTransportProtocol()) {
         case IP_PROT_UDP: {
-            UDPPacket *storedUdp = check_and_cast<UDPPacket *>(storedDatagram->getEncapsulatedPacket());
-            UDPPacket *liveUdp = check_and_cast<UDPPacket *>(liveDatagram->getEncapsulatedPacket());
+            UDPHeader *storedUdp = check_and_cast<UDPHeader *>(storedDatagram->getEncapsulatedPacket());
+            UDPHeader *liveUdp = check_and_cast<UDPHeader *>(liveDatagram->getEncapsulatedPacket());
             if (!(compareUdpPacket(storedUdp, liveUdp))) {
                 return false;
             }
@@ -1165,7 +1165,7 @@ bool PacketDrillApp::compareDatagram(IPv4Datagram *storedDatagram, IPv4Datagram 
     return true;
 }
 
-bool PacketDrillApp::compareUdpPacket(UDPPacket *storedUdp, UDPPacket *liveUdp)
+bool PacketDrillApp::compareUdpPacket(UDPHeader *storedUdp, UDPHeader *liveUdp)
 {
     return (storedUdp->getSourcePort() == liveUdp->getSourcePort()) &&
         (storedUdp->getDestinationPort() == liveUdp->getDestinationPort());
