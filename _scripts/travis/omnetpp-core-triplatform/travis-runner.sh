4d3c21304aebcac7302eaf4e8ff65cbd5ef1e1d9
TÖRÖK Attila <torokati44@gmail.com>
2017-10-19 17:09:52 +0200

    Added the Dockerfile and shell script for the image used on Travis.

---
+++ _scripts/travis/omnetpp-core-triplatform/travis-runner.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+
+set -e
+
+# This script runs the jobs on Travis.
+# The following environment variables should be set to select which job to run:
+#    TODO
+
+
+export PATH="$OPP_PATH:$PATH"
+
+# this is where the inet repo is mounted into the container
+cd /$TRAVIS_REPO_SLUG
+
+make makefiles
+
+make MODE=$MODE -j $(nproc)
+
+POSTFIX=""
+if [ "$MODE" = "debug" ]; then
+    POSTFIX="_dbg"
+fi
+
+if [ "$RUN_FINGERPRINT_TESTS" = "yes" ]; then
+    cd tests/fingerprint
+    ./runDefaultTests.sh -e opp_run$POSTFIX
+fi
