66b3cb9312bb5d94043dd2a55c1476cbbbeb42c3
TÖRÖK Attila <torokati44@gmail.com>
2017-10-20 15:04:40 +0200

    Updated .travis.yml, added badges to README.md.

--- _scripts/travis/omnetpp-core-triplatform/travis-runner.sh
+++ _scripts/travis/omnetpp-core-triplatform/travis-runner.sh
@@ -1,16 +1,20 @@
 #!/bin/env bash
 
-set -e
-
 # This script runs the jobs on Travis.
-# The following environment variables should be set to select which job to run:
-#    OPP_PATH, MODE, RUN_FINGERPRINT_TESTS, TRAVIS_REPO_SLUG
-#    - TODO docs
+#
+# The following environment variables must be set when invoked:
+#    TARGET_PLATFORM       - must be one of "linux", "windows", "macosx"
+#    MODE                  - must be "debug" or "release"
+#    RUN_FINGERPRINT_TESTS - must be "yes" or "no"
+#    TRAVIS_REPO_SLUG      - this is provided by Travis, most likely contains "inet-framework/inet"
 
 
-export PATH="$OPP_PATH:$PATH"
+set -e # make the script exit with error if any executed command exits with error
 
-# this is where the inet repo is mounted into the container
+
+export PATH="/root/omnetpp-5.2-$TARGET_PLATFORM/bin:$PATH"
+
+# this is where the cloned INET repo is mounted into the container (as prescribed in /.travis.yml)
 cd /$TRAVIS_REPO_SLUG
 
 make makefiles
