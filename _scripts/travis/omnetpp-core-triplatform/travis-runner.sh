2dab3b4cdd2dc93496903c02d9b621254e312d24
TÖRÖK Attila <torokati44@gmail.com>
2017-10-24 14:31:03 +0200

    try enabling ccache on fingerprint jobs

--- _scripts/travis/omnetpp-core-triplatform/travis-runner.sh
+++ _scripts/travis/omnetpp-core-triplatform/travis-runner.sh
@@ -12,21 +12,26 @@
 set -e # make the script exit with error if any executed command exits with error
 
 
+ccache -s
+
+
 export PATH="/root/omnetpp-5.2-$TARGET_PLATFORM/bin:$PATH"
 
 # this is where the cloned INET repo is mounted into the container (as prescribed in /.travis.yml)
 cd /$TRAVIS_REPO_SLUG
-
 make makefiles
 
-make MODE=$MODE -j $(nproc)
-
-POSTFIX=""
-if [ "$MODE" = "debug" ]; then
-    POSTFIX="_dbg"
-fi
 
 if [ "$RUN_FINGERPRINT_TESTS" = "yes" ]; then
+    make MODE=$MODE USE_PRECOMPILED_HEADER=no -j $(nproc)
+
+    POSTFIX=""
+    if [ "$MODE" = "debug" ]; then
+        POSTFIX="_dbg"
+    fi
+
     cd tests/fingerprint
     ./runDefaultTests.sh -e opp_run$POSTFIX
+else
+    make MODE=$MODE -j $(nproc)
 fi
