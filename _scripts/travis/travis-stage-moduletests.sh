248f47cd1e8bcc34550f1e1e5311f2ced4d4d0a6
TÖRÖK Attila <torokati44@gmail.com>
2018-06-14 13:43:39 +0200

    travis: add experimental moduletests stage

---
+++ _scripts/travis/travis-stage-moduletests.sh
@@ -0,0 +1,57 @@
+#!/bin/env bash
+
+# This script runs the moduletests stage on Travis.
+#
+# The following environment variables must be set when invoked:
+#    MODE                   - must be "debug" or "release"
+#
+#    TRAVIS_REPO_SLUG       - this is provided by Travis, most likely has the
+#                             value "inet-framework/inet"
+
+
+set -e # make the script exit with error if any executed command exits with error
+
+echo -e "\nccache summary:\n"
+ccache -s
+echo -e ""
+
+export PATH="/root/omnetpp-5.4-linux/bin:/usr/lib/ccache:$PATH"
+
+# this is where the cloned INET repo is mounted into the container (as prescribed in /.travis.yml)
+cd /$TRAVIS_REPO_SLUG
+
+cp -r /root/nsc-0.5.3 3rdparty
+
+opp_featuretool enable VoIPStream VoIPStream_examples TCP_NSC TCP_lwIP
+
+# We have to explicitly enable diagnostics coloring to make ccache work,
+# since we redirect stderr here, but not in the build stage.
+# See https://github.com/ccache/ccache/issues/222
+echo -e "CFLAGS += -fcolor-diagnostics\n\n$(cat src/makefrag)" > src/makefrag
+
+# enable instrumentation for code coverage reporting (to make the CFLAGS the same, and ccache happy)
+echo -e "CFLAGS += -fprofile-instr-generate -fcoverage-mapping\n$(cat src/makefrag)" > src/makefrag
+echo -e "LDFLAGS += -fprofile-instr-generate -fcoverage-mapping\n$(cat src/makefrag)" > src/makefrag
+
+echo -e "\nBuilding (silently)...\n"
+make makefiles > /dev/null 2>&1
+make MODE=$MODE USE_PRECOMPILED_HEADER=no -j $(nproc) > /dev/null 2>&1
+
+echo -e "\nccache summary:\n"
+ccache -s
+
+if [ "$MODE" = "release" ]; then
+    export LLVM_PROFILE_FILE="/root/coverage_profiles/inet-%h-%p.profraw"
+    cp src/libINET.so /root/coverage_profiles
+fi
+
+echo -e "\nBuild finished, starting module tests..."
+# echo -e "Additional arguments passed to fingerprint test script: " $@ "\n"
+
+cd tests/module
+if [ "$MODE" = "debug" ]; then
+    ./runtest
+else
+    # TODO
+    #./fingerprints -e opp_run_release "$@"
+fi
