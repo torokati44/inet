f6599392cff83f2ef3f3edeaf6292e75e845cd29
Zoltan Bojthe <zbojthe@omnetpp.org>
2023-04-27 17:44:59 +0200

    VoipStreamSender: Use swr_alloc_set_opts2() instead of many av_opt_set_int()

--- src/inet/applications/voipstream/VoipStreamSender.cc
+++ src/inet/applications/voipstream/VoipStreamSender.cc
@@ -251,33 +251,28 @@ void VoipStreamSender::openSoundFile(const char *name)
     if (avcodec_open2(pEncoderCtx, pCodecEncoder, nullptr) < 0)
         throw cRuntimeError("could not open %s encoding codec!", codec);
 
-    if (pCodecCtx->sample_rate == sampleRate
-        && pCodecCtx->sample_fmt == pEncoderCtx->sample_fmt
-        && pCodecCtx->ch_layout.nb_channels == 1)
+    pReSampleCtx = nullptr;
+    if (pCodecCtx->sample_rate != sampleRate
+        || pCodecCtx->sample_fmt != pEncoderCtx->sample_fmt
+        || pCodecCtx->ch_layout.nb_channels != 1)
     {
-        pReSampleCtx = nullptr;
-    }
-    else {
-        pReSampleCtx = swr_alloc();
-        if (!pReSampleCtx)
-            throw cRuntimeError("error in av_audio_resample_init()");
-
         AVChannelLayout inChannelLayout = pCodecCtx->ch_layout;
         if (inChannelLayout.u.mask == 0)
             av_channel_layout_default(&inChannelLayout, pCodecCtx->ch_layout.nb_channels);
         ASSERT(inChannelLayout.u.mask != 0);
-        if (av_opt_set_int(pReSampleCtx, "in_channel_layout", inChannelLayout.u.mask, 0))
-            throw cRuntimeError("error in option setting of 'in_channel_layout'");
-        if (av_opt_set_int(pReSampleCtx, "in_sample_fmt", pCodecCtx->sample_fmt, 0))
-            throw cRuntimeError("error in option setting of 'in_sample_fmt'");
-        if (av_opt_set_int(pReSampleCtx, "in_sample_rate", pCodecCtx->sample_rate, 0))
-            throw cRuntimeError("error in option setting of 'in_sample_rate'");
-        if (av_opt_set_int(pReSampleCtx, "out_channel_layout", AV_CH_LAYOUT_MONO, 0))
-            throw cRuntimeError("error in option setting of 'out_channel_layout'");
-        if (av_opt_set_int(pReSampleCtx, "out_sample_fmt", pEncoderCtx->sample_fmt, 0))
-            throw cRuntimeError("error in option setting of 'out_sample_fmt'");
-        if (av_opt_set_int(pReSampleCtx, "out_sample_rate", sampleRate, 0))
-            throw cRuntimeError("error in option setting of 'out_sample_rate'");
+        AVSampleFormat inSampleFmt = pCodecCtx->sample_fmt;
+        int inSampleRate = pCodecCtx->sample_rate;
+        AVChannelLayout outChannelLayout = AV_CHANNEL_LAYOUT_MONO;
+        AVSampleFormat outSampleFmt = pEncoderCtx->sample_fmt;
+        int outSampleRate = sampleRate;
+        err = swr_alloc_set_opts2(&pReSampleCtx,
+                &outChannelLayout, outSampleFmt, outSampleRate,
+                &inChannelLayout, inSampleFmt, inSampleRate,
+                0, nullptr);
+        if (err < 0)
+            throw cRuntimeError("Error opening context, swr_alloc_set_opts2() returns (%d) %s", err, av_err2str(err));
+        if (!pReSampleCtx)
+            throw cRuntimeError("error in swr_alloc()");
         if (av_opt_set_int(pReSampleCtx, "internal_sample_fmt", AV_SAMPLE_FMT_FLTP, 0))
             throw cRuntimeError("error in option setting of 'internal_sample_fmt'");
 
